29aff3689a625b29935da47c068f906b7ae17c11
Dave Snyder <daverted@gmail.com>
2020-05-22 09:21:38 -0600

    add error handling

--- overops-pod-patcher/opp/index.js
+++ overops-pod-patcher/opp/index.js
@@ -31,93 +31,116 @@ app.use(async (ctx, next) => {
 
 router.get(apiPrefix + 'namespaces', async (ctx, next) => {
   await next();
-  await client.loadSpec();
 
-  ctx.type = 'json';
-  ctx.body = await client.api.v1.namespaces.get();
+  try {
+    await client.loadSpec();
+
+    ctx.type = 'json';
+    ctx.body = await client.api.v1.namespaces.get();
+
+  } catch (error) {
+    console.error('Error getting namespaces');
+    ctx.throw('500');
+  }
 });
 
 router.get(apiPrefix + 'namespaces/:namespace/deployments', async (ctx, next) => {
   await next();
-  await client.loadSpec();
 
-  const namespace = ctx.params.namespace;
+  try {
+    await client.loadSpec();
 
-  ctx.type = 'json';
-  ctx.body = await client.apis.apps.v1.namespaces(namespace).deployments().get();
+    const namespace = ctx.params.namespace;
+  
+    ctx.type = 'json';
+    ctx.body = await client.apis.apps.v1.namespaces(namespace).deployments().get();
+
+  } catch (error) {
+    console.error('Error getting deployments');
+    ctx.throw('500');
+  }
 });
 
 router.patch(apiPrefix + 'namespaces/:namespace/deployments/:deployment', async (ctx, next) => {
   await next();
-  await client.loadSpec();
 
-  const namespace = ctx.params.namespace;
-  const deployment = ctx.params.deployment;
-  const agent = ctx.request.body.agent;
-  const disable = ctx.request.body.disable;
+  try {
+    await client.loadSpec();
 
-  const javaToolOptions = (disable ? null : '-agentpath:/takipi/lib/libTakipiAgent.so');
-
-  let containers = ctx.request.body.containers;
-  containers.forEach(container => {
-    container.volumeMounts = [{
-      name: 'overops-agent',
-      mountPath: '/takipi'
-    }];
-    container.env.push({
-      name: 'JAVA_TOOL_OPTIONS',
-      value: javaToolOptions
+    const namespace = ctx.params.namespace;
+    const deployment = ctx.params.deployment;
+    const agent = ctx.request.body.agent;
+    const disable = ctx.request.body.disable;
+  
+    const javaToolOptions = (disable ? null : '-agentpath:/takipi/lib/libTakipiAgent.so');
+  
+    let containers = ctx.request.body.containers;
+    containers.forEach(container => {
+      container.volumeMounts = [{
+        name: 'overops-agent',
+        mountPath: '/takipi'
+      }];
+      container.env.push({
+        name: 'JAVA_TOOL_OPTIONS',
+        value: javaToolOptions
+      });
     });
-  });
-
-  ctx.type = 'json';
-  ctx.body = await client.apis.apps.v1.namespaces(namespace).deployments(deployment).patch({
-    body: {
-      spec: {
-        template: {
-          spec: {
-            volumes: [{
-              name: "overops-agent",
-              emptyDir: {}
-            }],
-            initContainers: [{
-              name: 'overops-agent',
-              image: 'overops/agent-sidecar:' + agent,
-              imagePullPolicy: 'IfNotPresent',
-              resources: {
-                requests: {
-                  cpu: '10m',
-                  memory: '10Mi'
-                },
-                limits: {
-                  cpu: '100m',
-                  memory: '100Mi'
-                }
-              },
-              volumeMounts: [{
+  
+    ctx.type = 'json';
+    ctx.body = await client.apis.apps.v1.namespaces(namespace).deployments(deployment).patch({
+      body: {
+        spec: {
+          template: {
+            spec: {
+              volumes: [{
+                name: "overops-agent",
+                emptyDir: {}
+              }],
+              initContainers: [{
                 name: 'overops-agent',
-                mountPath: '/takipi'
-              }]
-            }],
-            containers: containers
+                image: 'overops/agent-sidecar:' + agent,
+                imagePullPolicy: 'IfNotPresent',
+                resources: {
+                  requests: {
+                    cpu: '10m',
+                    memory: '10Mi'
+                  },
+                  limits: {
+                    cpu: '100m',
+                    memory: '100Mi'
+                  }
+                },
+                volumeMounts: [{
+                  name: 'overops-agent',
+                  mountPath: '/takipi'
+                }]
+              }],
+              containers: containers
+            }
           }
         }
       }
-    }
-  });
+    });
+
+  } catch (error) {
+    console.error('Error patching deployment');
+    ctx.throw('500');
+  }
 });
 
 router.get(apiPrefix + 'repositories/overops/agent-sidecar/tags', async (ctx, next) => {
   await next();
-  await client.loadSpec();
 
-  // try {
+  try {
+    await client.loadSpec();
 
+    // get tags for overops/agent-sidecar from docker hub
     const response = await fetch('https://registry.hub.docker.com/v1/repositories/overops/agent-sidecar/tags');
     let data = await response.json();
 
     // sort by latest
     data.sort(function(a, b) {
+      // push alpine versions to the bottom
       if (a.name.startsWith('alpine') && !b.name.startsWith('alpine')) {
         return 1;
       }
@@ -136,12 +159,14 @@ router.get(apiPrefix + 'repositories/overops/agent-sidecar/tags', async (ctx, ne
     // remove 'latest' and 'debug' versions
     data = data.filter(tag => tag.name !== 'latest' && tag.name !== 'debug');
 
-  // } catch (error) {
-  //   console.log(error);
-  // }
+    // return tags
+    ctx.type = 'json';
+    ctx.body = data;
 
-  ctx.type = 'json';
-  ctx.body = data;
+  } catch (error) {
+    console.error('Error getting overops/agent-sidecar/tags from docker hub');
+    ctx.throw('500');
+  }
 });
 
 app
